494e7cabb42fb9a46b2b8700abffbef63f14cbef
Aleksey140569 <stranger140569@yandex.ru>
2022-11-14 22:27:41 +0300

    Добавил константы для кнопки и полей
    Чтоб потом было легче адаптировать скрипт для любой верстки добавил константы с индефикаторами полей.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,21 @@
 
 // Константы.
 
+// Константы индификаторов для вывода ошибок.
+
+const NAME_ERROR = "name-error";
+const HOURS_ERROR = "hours-error";
+const MINUTES_ERROR = "minutes-error";
+const SECONDS_ERROR = "seconds-error";
+
+// Константа инидификатора кнопки добавления записи в таблицу.
+
+const ADD_RECORD = "add";
+
 // Функции.
 
 // События.
 
 (function() {
-    
+  
 })();
